9194c9d4cdb01b38ff60a616269cbab3a0c98971
Mudassir Malik <Malikjrw147@gmail.com>
2023-11-07 16:38:13 +0500

    Update index.jsx

--- src/pages/postproject/index.jsx
+++ src/pages/postproject/index.jsx
@@ -5,6 +5,7 @@ import { debounce } from "@material-ui/core";
 import Axios from "../../Axios";
 import { Post } from "../../services/Api";
 import { uniqueArray } from "../../helpers";
+import { useUserContext } from "../../context/UserContext";
 // import ReactSummernote from 'react-summernote';
 // import 'react-summernote/dist/react-summernote.css'; 
 const Skills = ({ skills = [], onSkillSelect = () => { }, hide = () => { } }) => {
@@ -52,22 +53,21 @@ const SelectedSkills = ({ selectedSkills = [], onSkillSelect = () => { } }) => {
 }
 const PostProject = (props) => {
   const { enumsState } = useEnumsContext();
+  const { state } = useUserContext();
+  const currentUser = state.user;
   const [payload, setPayload] = React.useState({
     "_id": "",
-    "title": "Support 2 Title",
-    "description": "Support 2 Description...",
-    "supportType": 2,
-    "pricingType": 2,
-    "skills": [
-      "654743f34ee95d85831c1d1b",
-      "654743f94ee95d85831c1d1c"
-    ],
-    "deliveryStamp": 1701050895,
-    "deliveryType": 2,
-    "location": "California, USA",
+    "title": "",
+    "description": "",
+    "supportType": 0,
+    "pricingType": 0,
+    "skills": [],
+    "deliveryStamp": 0,
+    "deliveryType": 0,
+    "location": `${currentUser.addressLine}, ${currentUser.state}`,
     "fixedPrice": 0,
-    "hourlyMin": 30,
-    "hourlyMax": 80,
+    "hourlyMin": 0,
+    "hourlyMax": 0,
     "proposals": []
   });
   const [skills, setSkills] = React.useState([]);
@@ -78,13 +78,11 @@ const PostProject = (props) => {
       else return sk;
     }))
   }
-  function getUniqueArray(arr) {
-    const uniqueSet = new Set(arr.map(JSON.stringify));
-    return Array.from(uniqueSet).map(JSON.parse);
-  }
   const onChangeSkills = debounce(async (e) => {
     // console.log('[onChangeSkills]', e.target.value);
-    const data = await Post('api/v1/skill/search', { searchQuery: `${e.target.value}` });
+    const value = e.target.value;
+    if (value.length < 2) return null;
+    const data = await Post('api/v1/skill/search', { searchQuery: `${value}` });
     // console.log('[onChangeSkills].data', data);
     if (data.statusCode === 200) {
       setShow(true);
@@ -92,7 +90,15 @@ const PostProject = (props) => {
 
     }
   }, 1500)
-  const hide = () => setShow(false)
+  const hide = () => setShow(false);
+  const onChange = e => {
+    const id = e.target.id;
+    const value = e.target.value;
+    setPayload(pre => ({
+      ...pre,
+      [id]: value
+    }))
+  };
   return (
     <>
       {/* Breadcrumb */}
@@ -123,225 +129,112 @@ const PostProject = (props) => {
           <div className="row">
             <div className="col-md-12">
               <div className="select-project mb-4">
-                <form >
+                <form>
                   <div className="title-box widget-box">
-                    {/* Support Title */}
-                    <div className="title-content">
-                      <div className="title-detail">
-                        <h3>Title of Support Request</h3>
-                        <div className="form-group mb-0">
-                          <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter Support title"
-                            id="title"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                    {/* /Support Title */}
-                    {/* Category Content */}
-                    <div className="title-content">
-                      <div className="title-detail">
-                        <h3>Support Type</h3>
-                        <div className="form-group mb-0">
-                          <select className="form-control select">
-                            <option value={0}>Select</option>
-                            {(enumsState.SupportTypes || []).map((supportType, index) => {
-                              return <option value={supportType.value} key={`support-key-${index}`}>{supportType.text}</option>
-                            })}
+                    <div className="row">
 
-                          </select>
-                        </div>
-                      </div>
-                    </div>
-                    {/* /Category Content */}
-                    {/* Price Content */}
-                    <div className="title-content">
-                      <div className="title-detail">
-                        <h3>Pricing Type</h3>
-                        <div className="form-group price-cont mb-0" id="price_type">
-                          <select name="price" className="form-control select">
-                            <option value={0}>Select</option>
-                            {(enumsState.PricingTypes || []).map((pricingType, index) => {
-                              return <option value={pricingType.value} key={`support-key-${index}`}>{pricingType.text}</option>
-                            })}
-                          </select>
-                        </div>
-                        <div
-                          className="form-group mt-3"
-                          id="price_id"
-                          style={{ display: "none" }}
-                        >
-                          <div className="input-group">
-                            <div className="input-group-prepend">
-                              <button
-                                type="button"
-                                className="btn btn-white dropdown-toggle"
-                                data-bs-toggle="dropdown"
-                              >
-                                $
-                              </button>
-                              <div className="dropdown-menu">
-                                <Link className="dropdown-item" to="#">
-                                  Dollars
-                                </Link>
-                                <Link className="dropdown-item" to="#">
-                                  Euro
-                                </Link>
-                                <Link className="dropdown-item" to="#">
-                                  Bitcoin
-                                </Link>
-                              </div>
-                            </div>
+                      <div className="title-content col-md-4">
+                        <div className="title-detail">
+                          <h3>Title of Support Request</h3>
+                          <div className="form-group mb-0">
                             <input
                               type="text"
                               className="form-control"
-                              placeholder={20.0}
+                              placeholder="Enter Support title"
+                              id="title"
+                              onChange={onChange}
                             />
                           </div>
                         </div>
-                        <div
-                          className="form-group mt-3"
-                          id="hour_id"
-                          style={{ display: "none" }}
-                        >
-                          <div className="row">
-                            <div className="col-md-6 mb-2">
-                              <div className="input-group form-inline">
-                                <div className="input-group-prepend">
-                                  <button
-                                    type="button"
-                                    className="btn btn-white dropdown-toggle"
-                                    data-bs-toggle="dropdown"
-                                  >
-                                    $
-                                  </button>
-                                  <div className="dropdown-menu">
-                                    <Link className="dropdown-item" to="#">
-                                      Dollars
-                                    </Link>
-                                    <Link className="dropdown-item" to="#">
-                                      Euro
-                                    </Link>
-                                    <Link className="dropdown-item" to="#">
-                                      Bitcoin
-                                    </Link>
-                                  </div>
-                                </div>
-                                <input
-                                  type="text"
-                                  className="form-control mr-2"
-                                  placeholder={20.0}
-                                />{" "}
-                                <label> / hr</label>
-                              </div>
-                            </div>
-                            <div className="col-md-6">
-                              <div className="input-group form-inline">
-                                <label>For </label>{" "}
-                                <input
-                                  type="text"
-                                  className="form-control ml-2"
-                                  placeholder=" ( eg: 2 Weeks)"
-                                />
-                              </div>
-                            </div>
+                      </div>
+
+                      <div className="title-content col-md-4">
+                        <div className="title-detail">
+                          <h3>Support Type</h3>
+                          <div className="form-group mb-0">
+                            <select className="form-control select" id="supportType" onChange={onChange}>
+                              <option value={0}>Select</option>
+                              {(enumsState.SupportTypes || []).map((supportType, index) => {
+                                return <option value={supportType.value} key={`support-key-${index}`}>{supportType.text}</option>
+                              })}
+
+                            </select>
                           </div>
                         </div>
                       </div>
-                    </div>
-                    {/* /Price Content */}
-                    {/* Skills Content */}
-                    <div className="title-content">
-                      <div className="title-detail">
-                        <h3>Desired areas of expertise </h3>
-                        <div className="form-group mb-0">
-                          <input
-                            type="text"
-                            data-role="tagsinput"
-                            className="input-tags form-control"
-                            name="services"
-                            defaultValue=""
-                            id="services"
-                            placeholder="UX, UI, App Design, Wireframing, Branding"
-                            onChange={onChangeSkills}
-                          />
-                          {show && <Skills skills={skills} onSkillSelect={onSkillSelect} hide={hide} />}
-                          <SelectedSkills selectedSkills={skills.filter(s => s.selected)} onSkillSelect={onSkillSelect} />
-                        </div>
-                      </div>
-                    </div>
-                    {/* /Skills Content */}
-                    {/* Support Period Content */}
-                    <div className="title-content">
-                      <div className="title-detail">
-                        <h3>Support Needed By</h3>
-                        <div className="form-group mb-0" id="pro_period">
-                          <div className="radio">
-                            <label className="custom_radio">
-                              <input
-                                type="radio"
-                                defaultValue="period"
-                                name="period"
-                              />
-                              <span className="checkmark" /> ASAP
-                            </label>
-                          </div>
-                          <span className="checkmark" /> I have a specific iEP meeting or other important date scheduled by which time I would need support
 
-                          <div style={{ display: 'flex', flexDirection: 'row' }}>
+                      <div className="title-content col-md-4">
+                        <div className="title-detail">
+                          <h3>Support Needed By</h3>
+                          <div className="form-group mb-0">
+                            <select className="form-control select" id="supportType">
+                              <option value={0}>Select</option>
+                              {(enumsState.DeliveryTypes || []).map((supportType, index) => {
+                                return <option value={supportType.value} key={`support-key-${index}`}>{supportType.text}</option>
+                              })}
 
-                            <div className="radio">
-                              <label className="custom_radio">
-                                <input
-                                  type="radio"
-                                  defaultValue="period"
-                                  name="period"
-                                />
-                                <span className="checkmark" /> Yes
-                              </label>
-                            </div>
-
-                            <div className="radio">
-                              <label className="custom_radio">
-                                <input
-                                  type="radio"
-                                  defaultValue="period"
-                                  name="period"
-                                />
-                                <span className="checkmark" /> No
-                              </label>
-                            </div>
-                          </div>
-                          <div className="radio">
-                            <label className="custom_radio">
-                              <input
-                                type="radio"
-                                defaultValue="job"
-                                name="period"
-                                defaultChecked
-                              />
-                              <span className="checkmark" /> Job will Start On
-                            </label>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="col-md-4">
-                            <div className="filter-widget mb-0" id="period_date">
-                              <div className="cal-icon">
-                                <input
-                                  type="text"
-                                  className="form-control datetimepicker"
-                                  placeholder="Select Date"
-                                />
-                              </div>
-                            </div>
+                            </select>
                           </div>
                         </div>
                       </div>
+
                     </div>
-                    {/* /Support Period Content */}
+
+                    <div className="row">
+
+                      <div className="title-content col-md-4">
+                        <div className="title-detail">
+                          <h3>Pricing Type</h3>
+                          <div className="form-group price-cont mb-0" id="price_type">
+                            <select name="price" className="form-control select" id="pricingType" onChange={onChange}>
+                              <option value={0}>Select</option>
+                              {(enumsState.PricingTypes || []).map((pricingType, index) => {
+                                return <option value={pricingType.value} key={`support-key-${index}`}>{pricingType.text}</option>
+                              })}
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="title-content col-md-4">
+                        <div className="title-detail">
+                          <h3>Price</h3>
+                          <div className="input-group">
+                            <input
+                              type="text"
+                              className="form-control mr-2"
+                              placeholder={20.0}
+                              id="fixedPrice"
+                            />{" "}
+                            <label> / hr</label>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="title-content col-md-4">
+                        <div className="title-detail">
+                          <h3>Desired areas of expertise </h3>
+                          <div className="form-group mb-0">
+                            <input
+                              type="text"
+                              data-role="tagsinput"
+                              className="input-tags form-control"
+                              name="services"
+                              defaultValue=""
+                              id="services"
+                              placeholder="UX, UI, App Design, Wireframing, Branding"
+                              onChange={onChangeSkills}
+                            />
+                            {show && <Skills skills={skills} onSkillSelect={onSkillSelect} hide={hide} />}
+                            <SelectedSkills selectedSkills={skills.filter(s => s.selected)} onSkillSelect={onSkillSelect} />
+                          </div>
+                        </div>
+                      </div>
+
+                    </div>
+
+
+
                     <div className="title-content pb-0">
                       <div className="title-detail">
                         <h3>Describe the concern(s) for which you are seeking support </h3>
@@ -373,7 +266,7 @@ const PostProject = (props) => {
                         Your support request will be posted and accessible by eligible provoiders on our site.  While the providers are verified, it is best to avoid sharing any personally identifying details at this juncture.  Once you connect with a provider, you will have the ability to securely transmit personally identifiable information
                       </p>
                     </div>
-                    {/* /Support Title */}
+
                     <div className="row">
                       <div className="col-md-12 text-end">
                         <div className="btn-item">
@@ -384,14 +277,14 @@ const PostProject = (props) => {
                       </div>
                     </div>
                   </div>
-                  {/* Support Title */}
+
                 </form>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* /Page Content */}
+
     </>
   )
 
