4f9d1e2a766444c9aabf568284c05a916a123914
Mudassir Malik <Malikjrw147@gmail.com>
2023-11-22 18:06:06 +0500

    base url updated to dev server

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:80'
-// export const BASE_URL = 'https://spedify-staging-4b3eabb4964a.herokuapp.com'
+// export const BASE_URL = 'http://localhost:80'
+export const BASE_URL = 'https://spedify-dev-5c11dd64dfe3.herokuapp.com'
 export const ME = 'https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745';
 export const OTHER = 'https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-of-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg'
 
